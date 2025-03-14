913c185fdbb1131236fb43447df3103560876c0d
Venkataramana <venkychinna297@gmail.com>
2025-03-14 12:51:13 +0530

    Contact Section Done

--- my-portfolio-frontend/src/Pages/Contact/index.js
+++ my-portfolio-frontend/src/Pages/Contact/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import PageHeader from '../../Components/PageHeader';
 import { RiContactsFill } from "react-icons/ri";
+import { FaEnvelope, FaWhatsapp } from "react-icons/fa";
+import './index.css';
 
 const Contact = () => {
   return (
-    <section className="contact-container" id='contact'>
-      <PageHeader 
-        headerText = "Contact Me"
-        icon = {<RiContactsFill size={40}/>}
-      />
-    </section>
+    <>
+      <section className="contact-container" id='contact'>
+        <PageHeader 
+          headerText = "Contact Me"
+          icon = {<RiContactsFill size={40}/>}
+        />
+      </section>
+      <section class="contact-section">
+        <h2>Contact Me</h2>
+        <p>Feel free to reach out via email or WhatsApp.</p>
+
+        <div class="contact-buttons">
+          <a href="mailto:venkataramanakoppisetti25@gmail.com" class="contact-button email-button">
+          <FaEnvelope className="text-xl" />
+          </a>
+          <a href="https://wa.me/9652812397" class="contact-button whatsapp-button">
+          <FaWhatsapp className="text-xl" />
+          </a>
+        </div>
+
+        <div class="contact-form">
+          <form>
+            <label for="name">Name</label>
+            <input type="text" id="name" placeholder="Your Name"/>
+
+            <label for="email">Email</label>
+            <input type="email" id="email" placeholder="Your Email"/>
+
+            <label for="message">Message</label>
+            <textarea id="message" placeholder="Your Message"></textarea>
+
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+      </section>
+
+    </>
+    
   )
 }
 
